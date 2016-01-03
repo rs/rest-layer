019c94bdfe2067142c48ce950e693d6eaaa10cf6
Olivier Poitrey <rs@dailymotion.com>
2016-01-03 12:42:29 -0800

    Add some doc to compareEtag

--- rest/util.go
+++ rest/util.go
@@ -1,5 +1,9 @@
 package rest
 
+// compareEtag compares a client provided etag with a base etag. The client provided
+// etag may or may not have quotes while the base etag is never quoted. This loose
+// comparison of etag allows clients not stricly respecting RFC to send the etag with
+// or without quotes when the etag comes from, for instance, the API JSON response.
 func compareEtag(etag, baseEtag string) bool {
 	if etag == baseEtag {
 		return true
