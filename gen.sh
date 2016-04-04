b2ccc3868cfe7f60279ba6709427715c44e8a671
Olivier Poitrey <rs@dailymotion.com>
2016-04-04 15:53:50 -0700

    Refine feature list

--- gen.sh
+++ gen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git show master:README.md|marked --gfm > readme.html
+git show master:README.md|marked --gfm| \
+  sed -e 's/<li>\[ \] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" checked="checked" disabled="disabled">\&nbsp;/g
+          s/<li>\[x\] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" disabled="disabled">\&nbsp;/g' > readme.html
 sed -e '/<!-- Content -->/r readme.html' readme.tmpl > index.html
 rm -f readme.html
