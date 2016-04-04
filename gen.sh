d624f0bd7050286e710b89288353b6d3755dee49
Olivier Poitrey <rs@dailymotion.com>
2016-04-04 16:56:37 -0700

    Fix inverted checkbox

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git show master:README.md|marked --gfm --breaks > readme.html
-perl -pi -e 's/<li>\[ \] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" checked="checked" disabled="disabled">\&nbsp;/g,
-             s/<li>\[x\] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" disabled="disabled">\&nbsp;/g' readme.html
+perl -pi -e 's/<li>\[x\] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" checked="checked" disabled="disabled">\&nbsp;/g,
+             s/<li>\[ \] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" disabled="disabled">\&nbsp;/g' readme.html
 sed -e '/<!-- Content -->/r readme.html' readme.tmpl > index.html
 rm -f readme.html
