1441f4c17a22feaf066bf02d7ea79a61770dbf19
Olivier Poitrey <rs@dailymotion.com>
2016-04-14 19:06:13 -0700

    Update README

--- gen.sh
+++ gen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p doc
+git show master:doc/schema.png > doc/schema.png
 git show master:README.md|marked --gfm --breaks > readme.html
 perl -pi -e 's/<li>\[x\] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" checked="checked" disabled="disabled">\&nbsp;/g,
              s/<li>\[ \] /<li class="no-bullet"><input type="checkbox" class="task-list-item-checkbox" disabled="disabled">\&nbsp;/g' readme.html
