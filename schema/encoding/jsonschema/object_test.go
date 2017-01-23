750e78775eaf3f96a608bc3bef2c40ec4f001ba0
Sindre Myren <smyrman@gmail.com>
2017-01-23 13:55:36 +0100

    jsonschema: test cleanup and some new tests
    Clenaup:
    - Renamed some sub-tests / sub-benchmarks for increased consistency
    - Renamed/moved definitions of two "nested" schemas to testutil_test.go
    - Moves test utility types, vars and functions to testutil_test.go
    - Moves complex schema functions to benchmark_test.go
    
    New tests:
    - Object gets dedicated test file with a new test
    - New test/benchmark for simpleSchema
    - New benchmark for nestedObjects and simpleSchema

---
+++ schema/encoding/jsonschema/object_test.go
@@ -0,0 +1,51 @@
+package jsonschema_test
+
+import (
+	"testing"
+
+	"github.com/rs/rest-layer/schema"
+)
+
+var tinySchema = schema.Schema{
+	Fields: schema.Fields{
+		"name": {Validator: &schema.String{}},
+		"age":  {Validator: &schema.Integer{}},
+	},
+}
+
+const tinySchemaJSON = `{
+	"properties": {
+		"name": {"type": "string"},
+		"age": {"type": "integer"}
+	}
+}`
+
+func TestObjectValidatorEncode(t *testing.T) {
+	testCases := []encoderTestCase{
+		{
+			name: `Schema=nil`,
+			schema: schema.Schema{
+				Fields: schema.Fields{
+					"o": {
+						Validator: &schema.Object{},
+					},
+				},
+			},
+			expectError: "no schema defined for object",
+		},
+		{
+			name: `Schema=tiny`,
+			schema: schema.Schema{
+				Fields: schema.Fields{
+					"o": {
+						Validator: &schema.Object{Schema: &tinySchema},
+					},
+				},
+			},
+			customValidate: fieldValidator("o", tinySchemaJSON),
+		},
+	}
+	for i := range testCases {
+		testCases[i].Run(t)
+	}
+}
