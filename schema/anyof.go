26cdbb2341afdf27ec74e879258892e1b8fb575f
Tor-Inge J. Eriksen <tor@searis.no>
2018-03-19 10:13:29 +0100

    Use first matching serializer when serializing a field with AnyOf

--- schema/anyof.go
+++ schema/anyof.go
@@ -2,7 +2,9 @@ package schema
 
 import "errors"
 
-// AnyOf validates if any of the sub field validators validates.
+// AnyOf validates if any of the sub field validators validates. If any of the
+// sub field validators implements the FieldSerializer interface, the *first*
+// implementation which does not error will be used.
 type AnyOf []FieldValidator
 
 // Compile implements the Compiler interface.
@@ -27,4 +29,24 @@ func (v AnyOf) Validate(value interface{}) (interface{}, error) {
 	}
 	// TODO: combine errors.
 	return nil, errors.New("invalid")
+}
+
+// Serialize attempts to serialize the value using the first available
+// FieldSerializer which does not return an error. If no appropriate serializer
+// is found, the input value is returned.
+func (v AnyOf) Serialize(value interface{}) (interface{}, error) {
+	for _, serializer := range v {
+		s, ok := serializer.(FieldSerializer)
+		if !ok {
+			continue
+		}
+
+		v, err := s.Serialize(value)
+		if err != nil {
+			continue
+		}
+		return v, nil
+	}
+
+	return value, nil
 }
