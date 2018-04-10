1d1367b3414703d9fba5c8f331ab82db7a07404c
Sindre Røkenes Myren <sindre@searis.no>
2018-04-10 11:14:11 +0200

    mem: Let Clear handle query.Sort and query.Window
    This change lets the Storer Clear method handle query.Window (and thus
    also query.Sort) in the same way it's handled by the Find method.

--- resource/testing/mem/mem.go
+++ resource/testing/mem/mem.go
@@ -13,14 +13,16 @@ import (
 	"github.com/rs/rest-layer/schema/query"
 )
 
-// MemoryHandler is an example handler storing data in memory
+// MemoryHandler is an example handler storing data in memory.
 type MemoryHandler struct {
 	sync.RWMutex
-	// If latency is set, the handler will introduce an artificial latency on
-	// all operations
+
+	// If Latency is set, the handler will introduce an artificial latency on
+	// all operations.
 	Latency time.Duration
-	items   map[interface{}][]byte
-	ids     []interface{}
+
+	items map[interface{}][]byte
+	ids   []interface{}
 }
 
 func init() {
@@ -29,7 +31,7 @@ func init() {
 	gob.Register(time.Time{})
 }
 
-// NewHandler creates an empty memory handler
+// NewHandler creates an empty memory handler.
 func NewHandler() *MemoryHandler {
 	return &MemoryHandler{
 		items: map[interface{}][]byte{},
@@ -37,7 +39,7 @@ func NewHandler() *MemoryHandler {
 	}
 }
 
-// NewSlowHandler creates an empty memory handler with specified latency
+// NewSlowHandler creates an empty memory handler with specified latency.
 func NewSlowHandler(latency time.Duration) *MemoryHandler {
 	return &MemoryHandler{
 		Latency: latency,
@@ -46,7 +48,7 @@ func NewSlowHandler(latency time.Duration) *MemoryHandler {
 	}
 }
 
-// store serialize the item using gob and store it in the handler's items map
+// store serialize the item using gob and store it in the handler's items map.
 func (m *MemoryHandler) store(item *resource.Item) error {
 	var data bytes.Buffer
 	enc := gob.NewEncoder(&data)
@@ -57,7 +59,7 @@ func (m *MemoryHandler) store(item *resource.Item) error {
 	return nil
 }
 
-// fetch unserialize item's data and return a new item
+// fetch finds an item by ID and returns a new item with unserialized data.
 func (m *MemoryHandler) fetch(id interface{}) (*resource.Item, bool, error) {
 	data, found := m.items[id]
 	if !found {
@@ -71,7 +73,7 @@ func (m *MemoryHandler) fetch(id interface{}) (*resource.Item, bool, error) {
 	return &item, true, nil
 }
 
-// delete removes an item by this id with no look
+// delete removes an item by this id without locking.
 func (m *MemoryHandler) delete(id interface{}) {
 	delete(m.items, id)
 	// Remove id from id list
@@ -87,7 +89,7 @@ func (m *MemoryHandler) delete(id interface{}) {
 	}
 }
 
-// Insert inserts new items in memory
+// Insert inserts new items in memory.
 func (m *MemoryHandler) Insert(ctx context.Context, items []*resource.Item) (err error) {
 	m.Lock()
 	defer m.Unlock()
@@ -109,7 +111,7 @@ func (m *MemoryHandler) Insert(ctx context.Context, items []*resource.Item) (err
 	return err
 }
 
-// Update replace an item by a new one in memory
+// Update replace an item by a new one in memory.
 func (m *MemoryHandler) Update(ctx context.Context, item *resource.Item, original *resource.Item) (err error) {
 	m.Lock()
 	defer m.Unlock()
@@ -124,15 +126,12 @@ func (m *MemoryHandler) Update(ctx context.Context, item *resource.Item, origina
 		if original.ETag != o.ETag {
 			return resource.ErrConflict
 		}
-		if err := m.store(item); err != nil {
-			return err
-		}
-		return nil
+		return m.store(item)
 	})
 	return err
 }
 
-// Delete deletes an item from memory
+// Delete deletes an item from memory.
 func (m *MemoryHandler) Delete(ctx context.Context, item *resource.Item) (err error) {
 	m.Lock()
 	defer m.Unlock()
@@ -153,21 +152,16 @@ func (m *MemoryHandler) Delete(ctx context.Context, item *resource.Item) (err er
 	return err
 }
 
-// Clear clears all items from the memory store matching the lookup
+// Clear clears all items from the memory store matching q.
 func (m *MemoryHandler) Clear(ctx context.Context, q *query.Query) (total int, err error) {
 	m.Lock()
 	defer m.Unlock()
 	err = handleWithLatency(m.Latency, ctx, func() error {
-		ids := make([]interface{}, len(m.ids))
-		copy(ids, m.ids)
-		for _, id := range ids {
-			item, _, err := m.fetch(id)
-			if err != nil {
-				return err
-			}
-			if !q.Predicate.Match(item.Payload) {
-				continue
-			}
+		list, err := m.find(ctx, q)
+		if err != nil {
+			return err
+		}
+		for _, item := range list.Items {
 			m.delete(item.ID)
 			total++
 		}
@@ -176,49 +170,52 @@ func (m *MemoryHandler) Clear(ctx context.Context, q *query.Query) (total int, e
 	return total, err
 }
 
-// Find items from memory matching the provided lookup
+// Find items from memory matching the q.
 func (m *MemoryHandler) Find(ctx context.Context, q *query.Query) (list *resource.ItemList, err error) {
 	m.RLock()
 	defer m.RUnlock()
-	err = handleWithLatency(m.Latency, ctx, func() error {
-		items := []*resource.Item{}
-		// Apply filter
-		for _, id := range m.ids {
-			item, _, err := m.fetch(id)
-			if err != nil {
-				return err
-			}
-			if !q.Predicate.Match(item.Payload) {
-				continue
-			}
-			items = append(items, item)
-		}
-		// Apply sort
-		if len(q.Sort) > 0 {
-			s := sortableItems{q.Sort, items}
-			sort.Sort(s)
-		}
-		// Apply pagination
-		total := len(items)
-		if q.Window == nil {
-			list = &resource.ItemList{Total: total, Items: items}
-			return nil
-		}
-		end := total
-		start := q.Window.Offset
 
-		if q.Window.Limit >= 0 {
-			end = start + q.Window.Limit
-			if end > total-1 {
-				end = total
-			}
-		}
-		if start > total-1 {
-			start = 0
-			end = 0
-		}
-		list = &resource.ItemList{Total: total, Items: items[start:end]}
-		return nil
+	err = handleWithLatency(m.Latency, ctx, func() error {
+		list, err = m.find(ctx, q)
+		return err
 	})
 	return list, err
+}
+
+func (m *MemoryHandler) find(ctx context.Context, q *query.Query) (*resource.ItemList, error) {
+	// Fetch all items matching the filter
+	list := resource.ItemList{Items: []*resource.Item{}}
+	for _, id := range m.ids {
+		item, _, err := m.fetch(id)
+		if err != nil {
+			return nil, err
+		}
+		if !q.Predicate.Match(item.Payload) {
+			continue
+		}
+		list.Items = append(list.Items, item)
+	}
+	list.Total = len(list.Items)
+
+	// Apply sort
+	if len(q.Sort) > 0 {
+		s := sortableItems{q.Sort, list.Items}
+		sort.Sort(s)
+	}
+	// Apply pagination
+	if q.Window != nil {
+		list.Limit = q.Window.Limit
+		list.Offset = q.Window.Offset
+
+		if list.Offset > list.Total-1 {
+			list.Items = nil
+		} else {
+			list.Items = list.Items[list.Offset:]
+			if list.Limit >= 0 && list.Limit < len(list.Items) {
+				list.Items = list.Items[:list.Limit]
+			}
+		}
+
+	}
+	return &list, nil
 }
