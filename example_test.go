bc8636e6d443ae6560505256da86f585a7bf0b0d
Sindre Røkenes Myren <sindre@searis.no>
2018-11-30 21:02:56 +0100

    Converting to go 1.11 modules
    Removes vendored version of hysterix.
    
    Initattes two new go module roots:
    - One for the git root
    - One for the examples/ sub-directory
    
    Change example_test.go to rely on fewer external dependenceis.
    
    Updates Travis Go version to 1.11, and enables go build cache for faster
    CI runs.

--- example_test.go
+++ example_test.go
@@ -1,26 +1,95 @@
-// +build go1.7
-
 package restlayer
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 
-	"github.com/justinas/alice"
 	"github.com/rs/cors"
-	"github.com/rs/rest-layer/resource/testing/mem"
+
 	"github.com/rs/rest-layer/resource"
+	"github.com/rs/rest-layer/resource/testing/mem"
 	"github.com/rs/rest-layer/rest"
 	"github.com/rs/rest-layer/schema"
-	"github.com/rs/xaccess"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/hlog"
-	"github.com/rs/zerolog/log"
 )
 
+// ResponseRecorder extends http.ResponseWriter with the ability to capture
+// the status and number of bytes written
+type ResponseRecorder struct {
+	http.ResponseWriter
+
+	statusCode int
+	length     int
+}
+
+// NewResponseRecorder returns a ResponseRecorder that wraps w.
+func NewResponseRecorder(w http.ResponseWriter) *ResponseRecorder {
+	return &ResponseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
+}
+
+// Write writes b to the underlying response writer and stores how many bytes
+// have been written.
+func (w *ResponseRecorder) Write(b []byte) (n int, err error) {
+	n, err = w.ResponseWriter.Write(b)
+	w.length += n
+	return
+}
+
+// WriteHeader stores and writes the HTTP status code.
+func (w *ResponseRecorder) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+// StatusCode returns the status-code written to the response or 200 (OK).
+func (w *ResponseRecorder) StatusCode() int {
+	if w.statusCode == 0 {
+		return http.StatusOK
+	}
+	return w.statusCode
+}
+
+func AccessLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rec := NewResponseRecorder(w)
+
+		next.ServeHTTP(rec, r)
+		status := rec.StatusCode()
+		length := rec.length
+
+		// In this example we use the standard library logger. Structured logs
+		// may prove more parsable in a production environment.
+		log.Printf("D! Served HTTP Request %s %s with Response %d %s [%d bytes] in %d ms",
+			r.Method,
+			r.URL,
+			status,
+			http.StatusText(status),
+			length,
+			time.Since(start).Nanoseconds()/1e6,
+		)
+	})
+}
+
+var logLevelPrefixes = map[resource.LogLevel]string{
+	resource.LogLevelFatal: "E!",
+	resource.LogLevelError: "E!",
+	resource.LogLevelWarn:  "W!",
+	resource.LogLevelInfo:  "I!",
+	resource.LogLevelDebug: "D!",
+}
+
 func Example() {
+	// Configure a log-addapter for the resource pacakge.
+	resource.LoggerLevel = resource.LogLevelDebug
+	resource.Logger = func(ctx context.Context, level resource.LogLevel, msg string, fields map[string]interface{}) {
+		fmt.Printf("%s %s %v", logLevelPrefixes[level], msg, fields)
+	}
+
 	var (
 		// Define a user resource schema
 		user = schema.Schema{
@@ -115,12 +184,12 @@ func Example() {
 		}
 	)
 
-	// Create a REST API root resource
+	// Create a REST API root resource.
 	index := resource.NewIndex()
 
 	// Add a resource on /users[/:user_id]
 	users := index.Bind("users", user, mem.NewHandler(), resource.Conf{
-		// We allow all REST methods
+		// We allow all REST methods.
 		// (rest.ReadWrite is a shortcut for []rest.Mode{Create, Read, Update, Delete, List})
 		AllowedModes: resource.ReadWrite,
 	})
@@ -132,53 +201,32 @@ func Example() {
 		AllowedModes: []resource.Mode{resource.Read, resource.List, resource.Create, resource.Delete},
 	})
 
-	// Add a friendly alias to public posts
+	// Add a friendly alias to public posts.
 	// (equivalent to /users/:user_id/posts?filter={"public":true})
 	posts.Alias("public", url.Values{"filter": []string{"{\"public\"=true}"}})
 
-	// Create API HTTP handler for the resource graph
+	// Create API HTTP handler for the resource graph.
+	var api http.Handler
 	api, err := rest.NewHandler(index)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Invalid API configuration")
+		log.Printf("E! Invalid API configuration: %s", err)
+		os.Exit(1)
 	}
-
-	// Init an alice handler chain (use your preferred one)
-	c := alice.New()
-
-	// Install a logger
-	c = c.Append(hlog.NewHandler(log.With().Logger()))
-	c = c.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
-		hlog.FromRequest(r).Info().
-			Str("method", r.Method).
-			Str("url", r.URL.String()).
-			Int("status", status).
-			Int("size", size).
-			Dur("duration", duration).
-			Msg("")
-	}))
-	c = c.Append(hlog.RequestHandler("req"))
-	c = c.Append(hlog.RemoteAddrHandler("ip"))
-	c = c.Append(hlog.UserAgentHandler("ua"))
-	c = c.Append(hlog.RefererHandler("ref"))
-	c = c.Append(hlog.RequestIDHandler("req_id", "Request-Id"))
-	resource.LoggerLevel = resource.LogLevelDebug
-	resource.Logger = func(ctx context.Context, level resource.LogLevel, msg string, fields map[string]interface{}) {
-		zerolog.Ctx(ctx).WithLevel(zerolog.Level(level)).Fields(fields).Msg(msg)
-	}
-
-	// Log API access
-	c = c.Append(xaccess.NewHandler())
 
 	// Add CORS support with passthrough option on so rest-layer can still
-	// handle OPTIONS method
-	c = c.Append(cors.New(cors.Options{OptionsPassthrough: true}).Handler)
+	// handle OPTIONS method.
+	api = cors.New(cors.Options{OptionsPassthrough: true}).Handler(api)
 
-	// Bind the API under /api/ path
-	http.Handle("/api/", http.StripPrefix("/api/", c.Then(api)))
+	// Wrap the api & cors handler with an access log middleware.
+	api = AccessLog(api)
 
-	// Serve it
-	log.Info().Msg("Serving API on http://localhost:8080")
+	// Bind the API under the /api/ path.
+	http.Handle("/api/", http.StripPrefix("/api/", api))
+
+	// Serve it.
+	log.Printf("I! Serving API on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal().Err(err).Msg("")
+		log.Printf("E! Cannot serve API: %s", err)
+		os.Exit(1)
 	}
 }
